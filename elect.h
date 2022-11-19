b53c35dd22726894ebd67a80c054b2aa6dd88199
kelvin <edithmukoro3@gmail.com>
2022-11-19 23:04:33 +0100

    header file
    main functions and macros

---
+++ elect.h
@@ -0,0 +1,32 @@
+#ifndef ELECT_H
+#define ELECT_H
+
+#include<stdio.h>
+
+/*==================*/
+/*      MACROS      */
+/*==================*/
+#define CANDIDATE_COUNT
+
+#define CANDIDATE1 "Peter Obi"
+#define CANDIDATE2 "Bola Tinibu"
+#define CANDIDATE3 "Atiku Abubakar"
+#define CANDIDATE4 "Omoyele Sowore"
+
+/*==================*/
+/* Global Variables */
+/*==================*/
+int votesCount1=0, 
+	votesCount2=0, 
+	votesCount3=0, 
+	votesCount4=0, 
+	spoiledtvotes=0;
+
+/*==================*/
+/*  MAIN FUNCTIONS  */
+/*==================*/
+void castVote();
+void votesCount();
+void getLeadingCandidate();
+
+#endif  /* ELECT_H */
